13f4e844057baaf14b06d900323ad1bb4f3eb070
SamuraiT <4782857+SamuraiT@users.noreply.github.com>
2023-01-26 18:47:04 +0900

    feat: Add multicall feature

--- 01-defender-meta-txs/contracts/Registry.sol
+++ 01-defender-meta-txs/contracts/Registry.sol
@@ -3,22 +3,24 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
 
-contract Registry is ERC2771Context {  
-  event Registered(address indexed who, string name);
 
-  mapping(address => string) public names;
-  mapping(string => address) public owners;
+contract Registry is ERC2771Context, Multicall {
+    event Registered(address indexed who, string name);
 
-  constructor(MinimalForwarder forwarder) // Initialize trusted forwarder
-    ERC2771Context(address(forwarder)) {
-  }
+    mapping(address => string) public names;
+    mapping(string => address) public owners;
 
-  function register(string memory name) external {
-    require(owners[name] == address(0), "Name taken");
-    address owner = _msgSender(); // Changed from msg.sender
-    owners[name] = owner;
-    names[owner] = name;
-    emit Registered(owner, name);
-  }
+    constructor(
+        MinimalForwarder forwarder // Initialize trusted forwarder
+    ) ERC2771Context(address(forwarder)) {}
+
+    function register(string memory name) public {
+        require(owners[name] == address(0), "Name taken");
+        address owner = _msgSender(); // Changed from msg.sender
+        owners[name] = owner;
+        names[owner] = name;
+        emit Registered(owner, name);
+    }
 }
